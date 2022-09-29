08eade89fd0cb903f61680b97faa3c7758947869
Vlad <kirito_22223@mail.ru>
2022-09-29 19:08:19 +0300

    main commit

---
+++ advertisements/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from advertisements.models import Advertisement
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    pass
+
+
